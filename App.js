822ddfff9faa0057c046a81560cca76172588767
Lena <z.otig@yahoo.com>
2022-10-10 19:38:09 -0700

    img add

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Pressed the text");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={handlePress}>
+        Open up App.js to start working on your app!!
+      </Text>
+      <Image
+        source={{ width: 200, height: 200, uri: "https://picsum.photos/200" }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
